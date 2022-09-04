44d33ac5acc972a233bd4f775fa9963c2725ed31
jianhui <jchenk@live.com>
2022-09-04 09:25:14 +0800

    优化积分逻辑

--- shop/signals/receivers.py
+++ shop/signals/receivers.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, pre_delete
+from django.db.models.signals import post_save, pre_delete, post_delete
 from django.dispatch import receiver
 
 from shop.models import ExchangeLog, PointLog, PointReport
@@ -29,19 +29,30 @@ def on_exchangelog_created(sender, instance: ExchangeLog, created, **kwargs):
 
 @receiver(post_save, sender=PointLog, dispatch_uid='on_pointlog_created')
 def on_pointlog_created(sender, instance: PointLog, created, **kwargs):
-    """接收信号：积分变动"""
+    """接收信号：积分记录新增"""
 
     if created:
-        try:
-            # logger.warning('===== 积分变动后，更新积分统计 =====')
-            from django.db.models import Sum
-            from django.utils import timezone
-            points_sum = PointLog.objects.filter(student=instance.student).aggregate(points_sum=Sum('points'))
-            points = points_sum['points_sum'] or 0
-            PointReport.objects.update_or_create(
-                student=instance.student,
-                defaults={'points': points, 'updated_at': timezone.now()},
-            )
-        except Exception as e:
-            logger.error('===== 积分变动后，更新积分统计时发生错误 =====')
-            logger.error(str(e))
+        from django.db.models import Sum
+        from django.utils import timezone
+        
+        points_sum = PointLog.objects.filter(student=instance.student).aggregate(points_sum=Sum('points'))
+        points = points_sum['points_sum'] or 0
+        PointReport.objects.update_or_create(
+            student=instance.student,
+            defaults={'points': points, 'updated_at': timezone.now()},
+        )
+
+
+@receiver(post_delete, sender=PointLog, dispatch_uid='on_pointlog_deleted')
+def on_pointlog_deleted(sender, instance: PointLog, **kwargs):
+    """接收信号：积分记录删除"""
+
+    from django.db.models import Sum
+    from django.utils import timezone
+    
+    points_sum = PointLog.objects.filter(student=instance.student).aggregate(points_sum=Sum('points'))
+    points = points_sum['points_sum'] or 0
+    PointReport.objects.update_or_create(
+        student=instance.student,
+        defaults={'points': points, 'updated_at': timezone.now()},
+    )
--- teaching/signals/receivers.py
+++ teaching/signals/receivers.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, pre_delete
+from django.db.models.signals import post_save, pre_delete, post_delete
 from django.dispatch import receiver
 
 from teaching.models import StudentSignin
@@ -13,18 +13,26 @@ def on_studentsignin_created(sender, instance: StudentSignin, created, **kwargs)
     """接收信号：学生签到"""
     
     if created:
-        try:
-            # logger.warning('===== 学生签到后，增加积分 =====')
-            from shop.models import PointLog
-            from setting.models import get_setting_value
+        from shop.models import PointLog
+        from setting.models import get_setting_value
 
-            points = float(get_setting_value('signin_points', 10))
-            PointLog.objects.get_or_create(
-                student=instance.student,
-                log_type=PointLog.LogType.SIGNIN,
-                remark='Timetable.pk=%s'%str(instance.timetable.pk),
-                defaults={'points': points},
-            )
-        except Exception as e:
-            logger.error('===== 学生签到后，增加积分时发生错误 =====')
-            logger.error(str(e))
+        points = float(get_setting_value('signin_points', 10))
+        PointLog.objects.get_or_create(
+            student=instance.student,
+            log_type=PointLog.LogType.SIGNIN,
+            remark='Timetable.pk=%s'%str(instance.timetable.pk),
+            defaults={'points': points},
+        )
+
+
+@receiver(post_delete, sender=StudentSignin, dispatch_uid='on_studentsignin_deleted')
+def on_studentsignin_deleted(sender, instance: StudentSignin, **kwargs):
+    """接收信号：学生签到删除"""
+    
+    # logger.warning('===== 学生签到删除后，删除积分记录 =====')
+    from shop.models import PointLog
+    PointLog.objects.filter(
+        student=instance.student,
+        log_type=PointLog.LogType.SIGNIN,
+        remark='Timetable.pk=%s'%str(instance.timetable.pk),
+    ).delete()
